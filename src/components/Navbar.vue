9d400f39a3c2f7258b18c97084a9b0475d5af7ab
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-04-25 01:47:30 +0600

    router link state another way

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,7 +4,7 @@
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/contact">Contact</router-link>
+      <router-link :to="{name: 'Contact'}">Contact</router-link>
     </nav>
 
   </div>
